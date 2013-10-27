f56896613e4565c59f2c9fed91cac9af0c998dd5
harteware07@googlemail.com <harteware07@googlemail.com@0573e5d4-4abe-62b0-95d6-81ac19673f04>
2013-10-27 16:07:14 +0000

    created empty files

---
+++ arduinode/applicationLayer.h
@@ -0,0 +1,20 @@
+/* 
+ * Copyright (c) 2013, Jan Clement
+ * licenced under the GPL
+ *
+ * Author: Jan Clement <jan.clement@audiokits.de>
+ *
+ * This file is part of the arduiNode library
+ * http://code.google.com/p/arduinode/
+ */
+
+#ifndef APPLICATIONLAYER_H
+#define APPLICATIONLAYER_H
+
+#include "applicationLayer.h"
+#include "packetDef.h"
+
+
+
+
+#endif
